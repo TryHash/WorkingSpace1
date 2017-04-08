03d57f4299558b20d783ad6035be53c345216ffc
JD <junaidomer88@gmail.com>
2017-04-09 00:24:44 +0500

    did a little bit of work on Main layout

---
+++ JOfficials/JOfficials/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace JOfficials.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts/desktop")
+                .Include(
+                "~/Scripts/jquery-3.1.1.js",
+                "~/Scripts/bootstrap.js"
+                ).IncludeDirectory("~/Scripts", ".js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css/desktop")
+                .Include(
+                "~/Content/bootstrap.css"
+                ).IncludeDirectory("~/Content", ".css"));
+        }
+    }
+}
--- JOfficials/JOfficials/Global.asax.cs
+++ JOfficials/JOfficials/Global.asax.cs
@@ -5,7 +5,8 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
-
+using System.Web.Optimization;
+using JOfficials.App_Start;
 namespace JOfficials
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
@@ -19,6 +20,7 @@ namespace JOfficials
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
